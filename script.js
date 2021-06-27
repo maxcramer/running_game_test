b5ac9d25f0b9b332e9278b472e20d2bfb8b990ec
maxcramer <maximiliancramer@yahoo.com>
2021-06-27 16:34:48 +0100

    adding styling

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ var counter = 0;
 
 function jump() {
     if(runner.classList != "animate") {
-        runner.classList.add("animate")
-    }
+        runner.classList.add("animate");
+    };
     setTimeout(function() {
-        runner.classList.remove("animate")
-        counter++
-    }, 500)
-}
+        runner.classList.remove("animate");
+        counter++;
+    }, 500);
+};
 
 var lose = setInterval(function() {
-    runnerTop= parseInt(window.getComputedStyle(runner).getPropertyValue("top"))
-}, 10)
+    var runnerTop= parseInt(window.getComputedStyle(runner).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+    if(blockLeft < 20 && blockLeft > 0 && runnerTop >= 130) {
+        obstacle.style.animation = "none";
+        obstacle.style.display="none";
+        alert("Score: "+ counter);
+        counter = 0;
+    };
+}, 10);
